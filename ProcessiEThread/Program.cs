0ae2a00224576b52acfc10b5e98592290100cee4
studenti <studenti@rosselli.loc>
2019-10-22 09:31:59 +0200

    Aggiungere i file di progetto.

---
+++ ProcessiEThread/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ProcessiEThread
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread myThread = new Thread(() =>
+            {
+                Console.WriteLine("MyThread è iniziato");
+  
+                Thread.Sleep(1000);
+                Console.WriteLine("MyThread è iniziato");
+  
+            });
+
+            // Esecuzione di MyThread 
+            myThread.Start();
+
+            Thread.Sleep(500);
+            Console.WriteLine("Main Thread");
+
+            Console.ReadLine();
+                
+        }
+    }
+}
